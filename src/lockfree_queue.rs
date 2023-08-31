539b5f457650926d0c1f78ec4215a8ce3ff6c70e
soloist-v <w1053904672@163.com>
2023-08-31 16:00:20 +0800

    update

--- src/lockfree_queue.rs
+++ src/lockfree_queue.rs
@@ -9,7 +9,7 @@ use crossbeam_utils::CachePadded;
 /// 也无法通过不可变引用修改内部数据
 /// 如果想要修改内部数据就必须在包一层Mutex，这也是完全符合安全原则的
 /// 因此如果想要使用就必须使用unsafe，此时安全由使用者确保
-/// 所以在下面的读写分离实现中，使用了Arcell实现内部可变。
+/// 所以在下面的读写分离实现中，使用了Arc实现内部可变。
 #[derive(Debug)]
 pub struct RingBuffer<T, const SIZE: usize = 4> {
     m_data: [Option<T>; SIZE],
@@ -43,35 +43,38 @@ impl std::fmt::Display for Error {
 
 impl std::error::Error for Error {}
 
+
 impl<T, const SIZE: usize> RingBuffer<T, SIZE> {
-    fn push(&mut self, value: T) -> Result<(), Error> {
+    #[inline]
+    fn next_idx(cur: usize) -> usize {
+        (cur + 1) & (SIZE - 1)
+    }
+
+    #[inline]
+    fn ring_idx(cur: usize) -> usize {
+        cur & (SIZE - 1)
+    }
+
+    pub fn push(&mut self, value: T) -> Result<(), Error> {
         let head = self.idx_head.load(Ordering::Acquire);
-        let mut next_head = head + 1;
         let tail = self.idx_tail.load(Ordering::Acquire);
-        if next_head == SIZE {
-            next_head = 0;
-        }
+        let mut next_head = Self::next_idx(head);
         if next_head == tail {
             return Err(Error::Full);
         }
-
         self.m_data[head].replace(value);
         self.idx_head.store(next_head, Ordering::Release);
         Ok(())
     }
 
-    fn pop(&mut self) -> Result<T, Error> {
+    pub fn pop(&mut self) -> Result<T, Error> {
         let mut tail = self.idx_tail.load(Ordering::Acquire);
         let head = self.idx_head.load(Ordering::Acquire);
         if head == tail {
             return Err(Error::Empty);
         }
         let res = self.m_data[tail].take();
-        tail += 1;
-        if tail == SIZE {
-            tail = 0;
-        }
-        self.idx_tail.store(tail, Ordering::Release);
+        self.idx_tail.store(Self::next_idx(tail), Ordering::Release);
         match res {
             None => {
                 Err(Error::RuntimeError)
@@ -83,14 +86,14 @@ impl<T, const SIZE: usize> RingBuffer<T, SIZE> {
     }
 
     #[inline]
-    fn is_full(&self) -> bool {
+    pub fn is_full(&self) -> bool {
         let idx_tail = self.idx_tail.load(Ordering::Acquire);
         let idx_head = self.idx_head.load(Ordering::Acquire);
-        idx_tail == (idx_head + 1) % SIZE
+        idx_tail == Self::next_idx(idx_head)
     }
 
     #[inline]
-    fn is_empty(&self) -> bool {
+    pub fn is_empty(&self) -> bool {
         let idx_tail = self.idx_tail.load(Ordering::Acquire);
         let idx_head = self.idx_head.load(Ordering::Acquire);
         idx_head == idx_tail
